2f0593b678b440fa9d3bed6685293c465fe77a26
LeonardoTonon1234 <146954166+LeonardoTonon1234@users.noreply.github.com>
2024-10-22 11:24:41 +0000

    comite 4

--- webserver/src/main/java/br/dev/leonardo/webserver/WebserverApplication.java
+++ webserver/src/main/java/br/dev/leonardo/webserver/WebserverApplication.java
@@ -2,12 +2,17 @@ package br.dev.leonardo.webserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class WebserverApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebserverApplication.class, args);
+	}
+	@Bean
+	public CursosManager cursosManager(){
+		return new CursosManager();
 	}
 
 }
